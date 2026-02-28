d77c3114fece2f5dd0e58560fa2c9a641685135a
TouHouNo <czerwiiii@gmail.com>
2026-02-28 22:21:25 +0100

    ads-cli v0.1.5

--- Formula/ads-cli.rb
+++ Formula/ads-cli.rb
@@ -1,16 +1,16 @@
 class AdsCli < Formula
   desc "CLI for searching and working with Google Docs"
   homepage "https://github.com/czerwix/ads-cli"
-  version "0.1.4"
+  version "0.1.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/czerwix/ads-cli/releases/download/v#{version}/ads-macos-arm64.tar.gz"
-      sha256 "163a87a8abf5f4e925a9f67957eb89d43bee517238e5c87fbcff0c545f999a5b"
+      sha256 "071e0a9bf2eba857617e18d4c65515206cb88c0da35a1ae5b116ce8d11cd0d71"
     else
       url "https://github.com/czerwix/ads-cli/releases/download/v#{version}/ads-macos-x86_64.tar.gz"
-      sha256 "81a8ba64dabf61b04fa5becc4345a24f491540508bfe0167f413a69ee17c03f1"
+      sha256 "9f9e2e1f229ec8e3463b1d306add5c074fbb2ac97f31a56bbbec0f7b5bf264e9"
     end
   end
 
